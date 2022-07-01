6934e393e7cf73c1e0b5ae05779a8cf9e628d4f1
hasanerken <hasan.erken@gmail.com>
2022-07-01 15:20:20 +0300

    Initial commit

---
+++ src/pages/dashboard.vue
@@ -0,0 +1,17 @@
+<script setup>
+useTitle('Vital - Admin');
+</script>
+
+<template>
+  <q-page
+    class="flex flex-col flex-center space-y-30 bg-[#18161e] text-gray-50"
+  >
+    <div uppercase>Welcome to the Dashboard Page</div>
+    <Footer />
+  </q-page>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: admin
+</route>
---
+++ src/pages/index.vue
@@ -0,0 +1,12 @@
+<script setup>
+useTitle('Vital - Homepage');
+</script>
+
+<template>
+  <q-page
+    class="flex flex-col flex-center space-y-30 bg-[#404053] text-purple-50"
+  >
+    <VitalWelcome></VitalWelcome>
+    <Footer />
+  </q-page>
+</template>
